a6ea2877d84bc31e1e5d46792621e40b0498530a
esrenll <esrenll@gmail.com>
2020-04-24 05:08:02 +0900

    edit index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,25 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import { Jumbotron } from 'react-bootstrap'
 
 const ImasDef = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>home</h1>
+    <Jumbotron>
+      <h1>esrenll.github.io/gatsby</h1>
+      <p>
+        Hosted by GitHub
+        <br />
+        Built by{' '}
+        <a
+          href="https://github.com/esrenll/gatsby.esrenll.github.io"
+          target="_blank"
+        >
+          gatsby.esrenll.github.io
+        </a>
+      </p>
+    </Jumbotron>
   </Layout>
 )
 
